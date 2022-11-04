75c5397445bbdb5eba6969a5a4ce01d8b0cc9d66
Diegovano <36131808+Diegovano@users.noreply.github.com>
2022-11-04 21:41:16 +0000

    Complete task 2

---
+++ task2/f1_fsm_tb.cpp
@@ -0,0 +1,55 @@
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vf1_fsm.h"
+#include "vbuddy.cpp"
+
+#define MAX_SIM_CYC 1e6
+
+int main(int argc, char **argv, char **env) {
+    int simcyc;
+    int tick;
+
+    Verilated::commandArgs(argc, argv);
+
+    Vf1_fsm *top = new Vf1_fsm;
+
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    top->trace (tfp, 99);
+    tfp->open ("f1_fsm.vcd");
+
+    if (vbdOpen() != 1) return(-1);
+    vbdHeader("L3T2: F1 FSM");
+    vbdSetMode(1);
+
+    top->clk = 1;
+    top->en = 1;
+    top->rst = 0;
+
+    for (simcyc = 0; simcyc < MAX_SIM_CYC; simcyc++) {
+        for (tick = 0; tick < 2; tick++) {
+            tfp->dump (2 * simcyc + tick);
+            top->clk = !top->clk;
+            top->eval ();
+            if (vbdFlag()) {
+                top->en = 1;
+            }
+            else {
+                top->en = 0;
+            }
+        }
+
+        
+
+
+        vbdBar(top->dout & 0xFF);
+        // vbdHex(1, top->dout & 0xFF);
+        vbdCycle(simcyc);
+
+        if ((Verilated::gotFinish() || (vbdGetkey() == 'q'))) {
+            vbdClose();
+            tfp->close();
+            exit(0);
+        }
+    }
+}
---
+++ task2/obj_dir/Vf1_fsm___024root__DepSet_h6621a82a__0__Slow.cpp
@@ -0,0 +1,53 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See Vf1_fsm.h for the primary calling header
+
+#include "verilated.h"
+
+#include "Vf1_fsm___024root.h"
+
+VL_ATTR_COLD void Vf1_fsm___024root___initial__TOP__0(Vf1_fsm___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vf1_fsm__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vf1_fsm___024root___initial__TOP__0\n"); );
+    // Body
+    vlSelf->f1_fsm__DOT__current_state = 8U;
+}
+
+VL_ATTR_COLD void Vf1_fsm___024root___eval_initial(Vf1_fsm___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vf1_fsm__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vf1_fsm___024root___eval_initial\n"); );
+    // Body
+    Vf1_fsm___024root___initial__TOP__0(vlSelf);
+    vlSelf->__Vclklast__TOP__clk = vlSelf->clk;
+}
+
+void Vf1_fsm___024root___combo__TOP__0(Vf1_fsm___024root* vlSelf);
+
+VL_ATTR_COLD void Vf1_fsm___024root___eval_settle(Vf1_fsm___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vf1_fsm__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vf1_fsm___024root___eval_settle\n"); );
+    // Body
+    Vf1_fsm___024root___combo__TOP__0(vlSelf);
+}
+
+VL_ATTR_COLD void Vf1_fsm___024root___final(Vf1_fsm___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vf1_fsm__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vf1_fsm___024root___final\n"); );
+}
+
+VL_ATTR_COLD void Vf1_fsm___024root___ctor_var_reset(Vf1_fsm___024root* vlSelf) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vf1_fsm__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vf1_fsm___024root___ctor_var_reset\n"); );
+    // Body
+    vlSelf->rst = VL_RAND_RESET_I(1);
+    vlSelf->en = VL_RAND_RESET_I(1);
+    vlSelf->clk = VL_RAND_RESET_I(1);
+    vlSelf->dout = VL_RAND_RESET_I(8);
+    vlSelf->f1_fsm__DOT__current_state = 0;
+    vlSelf->f1_fsm__DOT__next_state = 0;
+}
